f22ba053d843c84b5e659b34745c0be3ff93db23
Ammaar Alam <ammaar@princeton.edu>
2024-10-15 21:08:21 -0400

    compile c code during build phase

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,10 +2,5 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-echo "Compiling C code..."
-cd backend
-make
-cd ..
-
 echo "Starting Gunicorn server..."
 gunicorn server.app:app
